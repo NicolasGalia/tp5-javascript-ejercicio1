024b671b56958f0ec9023787d22a6b0b69049e5a
Nicolas GaliaM <nicolasgaliam@gmail.com>
2022-08-15 21:38:01 -0300

    realizando el ejercicio numero 2 del tp5

--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -6,6 +6,33 @@
 // mostrarDatos: devuelve toda la información del objeto.
 // Luego crea la interfaz necesaria para que el usuario pueda crear un objeto persona, permitiendo ingresar las propiedades mediante un formulario, también agregar los botones “mostrar generación”, es “mayor de edad” e indicar en un alert el resultado de la función correspondiente.
 
+class Persona {
+ constructor (nombre, edad, DNI, sexo = (H,M), peso, altura, añoDeNacimiento){
+this.nombre = nombre; 
+this.edad = edad;
+this.DNI = DNI;
+this.sexo = sexo;
+this.peso = peso;
+this.altura = altura;
+this.añoDeNacimiento = añoDeNacimiento;
+ }
+mostrarGeneracion(){
+
+}
+mostrarDatos(){
+
+}
+
+}
+
+let generacion = ["generacion Z", "generacion Y", "generacion X", "baby boom", "Silent Generation"]
+
+let rasgoCaracteristico = ["irreverencia", "frustracion", "obsesion por el exito", "ambicion", "austeridad"]
+
+var persona1 = new Persona("Nicolas", 18, 41124900, "H", 59, 1,87, 2000);
+console.log(persona1);
+
+
 
 
 
